88c1019c78dcf564966613ec68e6f7806820f69e
Rodolphe POON <poon.rodolphe@hotmail.fr>
2019-10-05 18:02:30 +0200

    init commit

---
+++ assets/scripts/Game.js
@@ -0,0 +1,202 @@
+// Learn cc.Class:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {
+    ccclass,
+    property
+} = cc._decorator
+
+const converTileMapRefToNodeRef = ({
+    x,
+    y
+}, {
+    width,
+    x: tileX,
+    anchorX,
+    height,
+    y: tileY,
+    anchorY,
+    scaleX,
+    scaleY,
+}) => cc.v2((x - width * anchorX) * scaleX + tileX, (y - height * anchorY) * scaleY + tileY)
+
+const convertNodeRefToTileMapRef = ({
+    x,
+    y
+}, {
+    width,
+    x: tileX,
+    anchorX,
+    height,
+    y: tileY,
+    anchorY,
+    scaleX,
+    scaleY,
+}) => cc.v2(Math.round(((x - tileX) / scaleX) + width * anchorX), Math.round(((y - tileY) / scaleY) + height * anchorY))
+
+const convertTileMapRefToGridRef = ({
+    x,
+    y
+}, {
+    mapTileSize,
+    layerSize
+}) => cc.v2((Math.round(x) - mapTileSize.width / 2) / mapTileSize.width, -((Math.round(y) + mapTileSize.height / 2) / mapTileSize.height) + layerSize.height)
+
+const getTilePos = (position, tiledMap) => convertTileMapRefToGridRef(convertNodeRefToTileMapRef(position, tiledMap.node), {
+    mapTileSize: tiledMap.getTileSize(),
+    layerSize: tiledMap.getMapSize()
+})
+
+const SHATTERED_ICE_ID = 4455
+const HOLE_ID = 4263
+const ICE_ID = 4454
+const ICE_2_ID = 4646
+const ICE_WALL_ID = 4456
+const STAIRS_ID = 4264
+
+
+@ccclass
+class Controller extends cc.Component {
+
+
+    @property({
+        type: cc.Prefab
+
+    }) player = null
+    @property({
+        type: cc.Prefab
+
+    }) loseLabel = null
+    @property({
+        type: cc.Prefab
+
+    }) winLabel = null
+
+    onLoad() {
+        this.createPlayer()
+        this.createMap()
+    }
+
+    start() {
+        this.startGame()
+    }
+
+    createMap() {
+        this.tiledMap = this.node.getComponentInChildren(cc.TiledMap)
+        this.tiledLayer = this.tiledMap.getLayer('normal')
+    }
+
+
+    startGame() {
+        this.iceCount = 0
+        this.tiledLayer._tiles.forEach(tileID => {
+            if (tileID === ICE_ID || tileID === ICE_2_ID) {
+                this.iceCount += 1
+            }
+        });
+
+        this.playerNode.getComponent("player").checkCollision = (position, keyCode, step) => {
+            let futurPos
+            switch (keyCode) {
+                case cc.macro.KEY.left:
+                    futurPos = cc.v2(position.x - step, position.y)
+                    break;
+                case cc.macro.KEY.right:
+                    futurPos = cc.v2(position.x + step, position.y)
+                    break;
+                case cc.macro.KEY.up:
+                    futurPos = cc.v2(position.x, position.y + step)
+                    break;
+                case cc.macro.KEY.down:
+                    futurPos = cc.v2(position.x, position.y - step)
+                    break;
+                default:
+                    futurPos = position
+                    break;
+            }
+            let tilePos = getTilePos(futurPos, this.tiledMap)
+            let tileProps = this.tiledMap.getPropertiesForGID(this.tiledLayer.getTileGIDAt(tilePos))
+
+            return !tileProps.canWalkOn
+        }
+
+
+
+        this.playerNode.getComponent("player").onWalkEnd = (position) => {
+            let tilePos = getTilePos(position, this.tiledMap)
+            let tileProps = this.tiledMap.getPropertiesForGID(this.tiledLayer.getTileGIDAt(tilePos))
+            switch (tileProps.type) {
+                case 'ice':
+                    this.iceCount -= 1;
+                    this.tiledLayer.setTileGIDAt(SHATTERED_ICE_ID, tilePos)
+
+                    if (this.iceCount === 0) {
+                        this.unlockStairs()
+                    }
+
+                    break;
+                case "shattered Ice":
+                    this.tiledLayer.setTileGIDAt(HOLE_ID, tilePos)
+                    this.gameOver()
+                    break;
+                case "stairs":
+                    this.nextLevel()
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+
+    unlockStairs() {
+        let obj = this.tiledMap.getObjectGroup("objects").getObject('finish')
+        let nodeRefPos = converTileMapRefToNodeRef({
+            x: obj.x + obj.width / 2,
+            y: obj.y - obj.height / 2
+        }, this.tiledMap.node)
+        let tilePos = getTilePos(nodeRefPos, this.tiledMap)
+        this.tiledLayer.setTileGIDAt(STAIRS_ID, tilePos)
+    }
+    nextLevel() {
+        let win = cc.instantiate(this.winLabel);
+        win.zIndex = cc.macro.MAX_ZINDEX;
+        win.position = cc.v2(0, 260)
+        this.node.addChild(win);
+    }
+
+    gameOver() {
+        let gameOverLabel = cc.instantiate(this.loseLabel);
+        gameOverLabel.zIndex = cc.macro.MAX_ZINDEX;
+        gameOverLabel.position = cc.v2(0, 260)
+        this.node.addChild(gameOverLabel);
+        this.playerNode.getComponent("player").die()
+    }
+
+    createPlayer() {
+        if (!cc.isValid(this.playerNode)) {
+            this.playerNode = cc.instantiate(this.player);
+            this.playerNode.zIndex = cc.macro.MAX_ZINDEX;
+            this.node.addChild(this.playerNode);
+        }
+        let tilemap = this.node.getComponentInChildren(cc.TiledMap).node
+        let obj = this.node.getComponentInChildren(cc.TiledMap).getObjectGroup("objects").getObject('start')
+        this.playerNode.position = converTileMapRefToNodeRef({
+            x: obj.x + obj.width / 2,
+            y: obj.y - obj.height / 2
+        }, tilemap)
+    }
+
+
+
+
+
+
+}
---
+++ assets/scripts/player.js
@@ -0,0 +1,205 @@
+// Learn cc.Class:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle onWalkEnds:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-onWalkEnds.html
+//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-onWalkEnds.html
+const {
+    ccclass,
+    property
+} = cc._decorator
+
+
+
+@ccclass
+class Controller extends cc.Component {
+
+
+    @property
+    speed = 200
+    @property
+    step = 10
+    @property({
+        type: cc.SpriteFrame
+    }) upSprite = null
+    @property({
+        type: cc.SpriteFrame
+    }) downSprite = null
+    @property({
+        type: cc.SpriteFrame
+    }) leftSprite = null
+    @property({
+        type: cc.SpriteFrame
+    }) rightSprite = null
+
+
+
+
+
+    map = new Map()
+    directions = ["up", "down", "left", "right"]
+    direction = {
+        "up": cc.macro.KEY.up,
+        "down": cc.macro.KEY.down,
+        "left": cc.macro.KEY.left,
+        "right": cc.macro.KEY.right,
+    }
+    lastKeyPressed
+    isWalking = false
+    onWalkEnd
+    isDead = false
+
+    checkCollision = () => false
+
+
+    onKeyDown(e) {
+        this.map.set(e.keyCode, true)
+        if (!this.isWalking && !this.isDead) {
+            this.changeSprite(e.keyCode)
+
+            if (!this.checkCollision(this.node.getPosition(), e.keyCode, this.step))
+                this.go(e.keyCode)
+
+        }
+    }
+
+    changeSprite(key) {
+        switch (key) {
+            case cc.macro.KEY.left:
+                this.node.getComponent(cc.Sprite).spriteFrame = this.leftSprite
+                break;
+            case cc.macro.KEY.right:
+                this.node.getComponent(cc.Sprite).spriteFrame = this.rightSprite
+                break;
+            case cc.macro.KEY.up:
+                this.node.getComponent(cc.Sprite).spriteFrame = this.upSprite
+                break;
+            case cc.macro.KEY.down:
+                this.node.getComponent(cc.Sprite).spriteFrame = this.downSprite
+                break;
+            default:
+                break;
+
+        }
+    }
+
+    onLoad() {
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this)
+        this.node.getParent().on(cc.Node.EventType.MOUSE_DOWN, (e) => {
+
+            this.onKeyDown(this.getKeyCodeFromClickPos(e))
+        })
+        this.node.getParent().on(cc.Node.EventType.TOUCH_START, (e) => {
+            this.onKeyDown(this.getKeyCodeFromClickPos(e))
+        })
+    }
+
+    getKeyCodeFromClickPos(e) {
+        let click = cc.v2(this.node.getParent().convertToNodeSpaceAR(e.getLocationInView()).x, -this.node.getParent().convertToNodeSpaceAR(e.getLocationInView()).y)
+        let position = this.node.getPosition()
+        let direction
+
+        if (click.x > position.x - this.node.width && click.x < position.x + this.node.width && click.y > position.y + this.node.height && click.y < position.y + this.node.height + this.node.height * 2)
+            direction = "up"
+        else if (click.x > position.x - this.node.width && click.x < position.x + this.node.width && click.y < position.y - this.node.height && click.y > position.y - this.node.height - this.node.height * 2)
+            direction = "down"
+        else if (click.x > position.x + this.node.width && click.x < position.x + this.node.width + this.node.width * 2 && click.y > position.y - this.node.height && click.y < position.y + this.node.height)
+            direction = "right"
+        else if (click.x < position.x - this.node.width && click.x > position.x - this.node.width - this.node.width * 2 && click.y > position.y - this.node.height && click.y < position.y + this.node.height)
+            direction = "left"
+
+
+        return {
+            keyCode: cc.macro.KEY[direction]
+        }
+    }
+
+
+
+    start() {
+        console.log('on player start')
+    }
+    die = () => {
+        this.isDead = true
+        this.getComponent(cc.Animation).getAnimationState('die').play()
+        this.directions.forEach(d => {
+            this.getComponent(cc.Animation).stop(d)
+            this.getComponent(cc.Animation).setCurrentTime(0, d)
+        })
+
+    }
+
+
+
+    changeAnim(direction) {
+        this.getComponent(cc.Animation).getAnimationState(direction).play()
+        this.directions.filter(d => d !== direction).forEach(d => {
+            this.getComponent(cc.Animation).stop(d)
+            this.getComponent(cc.Animation).setCurrentTime(0, d)
+        })
+        //  this.lastKeyPressed = direction
+    }
+
+    go(key) {
+        switch (key) {
+            case cc.macro.KEY.left:
+                this.isWalking = true
+                this.changeAnim('left')
+                this.node.runAction(cc.sequence(cc.moveBy(0.5, -this.step, 0), cc.callFunc(() => {
+                    this.getComponent(cc.Animation).pause('left')
+                    this.getComponent(cc.Animation).setCurrentTime(0, 'left')
+                    this.isWalking = false
+                    this.onWalkEnd(this.node.position)
+                })))
+                break;
+            case cc.macro.KEY.right:
+                this.isWalking = true
+
+                this.changeAnim('right')
+                this.node.runAction(cc.sequence(cc.moveBy(0.5, this.step, 0), cc.callFunc(() => {
+                    this.getComponent(cc.Animation).pause('right')
+                    this.getComponent(cc.Animation).setCurrentTime(0, 'right')
+                    this.isWalking = false
+                    this.onWalkEnd(this.node.position)
+                })))
+
+                // this.node.setPosition(this.node.x += this.speed * dt, this.node.y)
+                break;
+            case cc.macro.KEY.up:
+                this.isWalking = true
+                this.changeAnim('up')
+                this.node.runAction(cc.sequence(cc.moveBy(0.5, 0, this.step), cc.callFunc(() => {
+                    this.getComponent(cc.Animation).pause('up')
+                    this.getComponent(cc.Animation).setCurrentTime(0, 'up')
+                    this.isWalking = false
+                    this.onWalkEnd(this.node.position)
+                })))
+
+                // this.node.setPosition(this.node.x, this.node.y += this.speed * dt)
+                break;
+            case cc.macro.KEY.down:
+                this.isWalking = true
+                this.changeAnim('down')
+                this.node.runAction(cc.sequence(cc.moveBy(0.5, 0, -this.step), cc.callFunc(() => {
+                    this.getComponent(cc.Animation).pause('down')
+                    this.getComponent(cc.Animation).setCurrentTime(0, 'down')
+                    this.isWalking = false
+                    this.onWalkEnd(this.node.position)
+
+                })))
+
+                // this.node.setPosition(this.node.x, this.node.y -= this.speed * dt)
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    update(dt) {}
+
+
+}
